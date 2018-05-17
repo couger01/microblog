431a92fbf5ed88b7ff0d1b2c0ec75d7cb2ac729d
Darkmind14 <couger01@luther.edu>
2018-05-17 00:57:23 -0500

    Part 10

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['couger01@luther.edu']
     POSTS_PER_PAGE = 25
